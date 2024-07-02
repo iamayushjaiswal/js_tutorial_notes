e295c1f5b6a1db4b5a414cb12e7dbcb462d7e401
Ayush Jaiswal <95422235+ayushjai28@users.noreply.github.com>
2024-07-02 13:55:59 +0530

    nullish coalescing

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -19,17 +19,17 @@ if (userEmail) {
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
+if (Object.keys(emptyObj).length === 0) {//checking object is emty or not 
     console.log("Object is empty");
 }
 
-// Nullish Coalescing Operator (??): null undefined
+// Nullish Coalescing Operator (??): null undefined  //operator to do safety check of null and undefined
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
+// val1 = 5 ?? 10    
+// val1 = null ?? 10    
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+val1 = null ?? 10 ?? 20 //the first value except null and undefined will be assigned
 
 
 
